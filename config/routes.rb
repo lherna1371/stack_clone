4e1ebcf266fd6fe0d3bc20fa29337534e8a92544
Luis Hernandez <lherna1371@gmail.com>
2013-09-18 17:53:38 -0500

    complete some database.yml edits and root edit

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ StackClone::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-
+root :to => "welcome#index"
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
