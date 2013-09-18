c8bcb40113c1281ae5639b700398d7e6395976f2
Luis Hernandez <lherna1371@gmail.com>
2013-09-18 16:19:27 -0500

    .

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+StackClone::Application.config.secret_key_base = 'cba73169f40f30c85f080a433a258a59bedf62ef5e60d2d01542e63be0978aafc09f7b22d4b21aca4da3d9e70e1b62392486f8449ba867204217cab4991f455f'
