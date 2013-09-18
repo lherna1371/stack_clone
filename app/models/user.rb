1f912cba6f9b3a4fc7cd49cfae8b53ebe73296b3
Eugene Filimonov <eugenefilimonov@gmail.com>
2013-09-18 17:19:43 -0500

    added bcrypt and implemented it

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,24 @@
+require 'bcrypt'
+
 class User < ActiveRecord::Base
+  include BCrypt
+	validates :handle, presence: true
+	validates :email, presence: true,uniqueness: true
+	validates :password_digest, presence: true
+
+	def self.authenticate(name, secret)
+    user = find_by_name(name)
+    if user && user.password == secret
+      return user
+    end
+  end
+
+  def password
+    @password = Password.new(password_digest)
+  end
+
+  def password=(secret)
+    @password = Password.create(secret)
+    self.password_digest = @password
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 StackClone::Application.routes.draw do
+  resources :user
+
+
+
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
