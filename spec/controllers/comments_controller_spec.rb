8788fdb2ade911a26afcfda10dea9f5136d4ca39
Javier <Ibarrola>
2013-09-19 08:47:12 -0500

    another commit

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -1,24 +1,13 @@
 require 'spec_helper'
 
 describe CommentsController do
-  describe "admin panel" do
-    it "welcome page requires http authentication" do
-      get :index
-      response.status.should eq 401
+  
+
+  describe "new view" do
+    it "displays title create a comment" do
+      visit new_answer_comment
+      page.should have_content "Comment on this answer:"
     end
 
-    it "welcome page username is 'geek' and password is 'jock'" do
-      user = 'geek'
-      pw   = 'jock'
-      request.env['HTTP_AUTHORIZATION'] = ActionController::HttpAuthentication::Basic.encode_credentials(user,pw)
-
-      get :index
-      response.status.should eq 200
-    end
-
-    it "non-welcome pages do not require http auth" do
-      get :new
-      response.status.should eq 200
-    end
   end
 end
