edf21774f3145f66e69932d0e1059a7f49105e6d
Nicholas <nicholas.lee.3@gmail.com>
2013-09-19 13:36:42 -0500

    added back ext

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+# These are extensions that must be enabled in order to support this database
+enable_extension "plpgsql"
+  
 ActiveRecord::Schema.define(version: 20130918235525) do
 
   create_table "answers", force: true do |t|
