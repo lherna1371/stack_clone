6f82ed89a035625fa491ceefdab2e9d65b1318ca
Javier <Ibarrola>
2013-09-19 15:24:24 -0500

    edited the create action to be used by questions and answers

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,16 +3,23 @@ class CommentsController < ApplicationController
 	def new
 		#@answer = Answer.find(params[:answer_id])
 		if params[:answer_id]
-			@resource = Answer.find
+			@resource = Answer.find(params[:answer_id])
 		elsif params[:question_id]
-			
-
+			@resource = Question.find(params[:question_id])
+		end
 	end
 
 	def create
-		@answer = Answer.find(params[:answer_id])
-		@question = Question.find(@answer.question_id)
-    @comment = @answer.comments.create(content: params[:comment][:content], user_id: current_user.id)
-    redirect_to @question
+
+
+		if params[:answer_id]
+			@answer = Answer.find(params[:answer_id])
+			@question = Question.find(@answer.question_id)
+	    	@comment = @answer.comments.create(content: params[:comment][:content], user_id: current_user.id)
+		elsif params[:question_id]
+			@question = Question.find(params[:question_id])
+	    	@comment = @question.comments.create(content: params[:comment][:content], user_id: current_user.id)
+		end
+    	redirect_to @question
 	end
 end
