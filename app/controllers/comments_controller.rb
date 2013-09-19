29aa22421c6c6cb0a5081e2f11e36c5fc6edc75c
Javier <Ibarrola>
2013-09-18 19:44:22 -0500

    fixed comment new view it controller and made the create controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,13 @@
 class CommentsController < ApplicationController
 
 	def new
+		@answer = Answer.find(params[:answer_id])
 	end
 
 	def create
+		@answer = Answer.find(params[:answer_id])
+    @comment = @answer.comments.create(params[:comment].merge({:user_id => current_user.id}))
+    redirect_to @answer
 	end
-	
+
 end
