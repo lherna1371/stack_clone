daebad32446184833f7e80c8a033d14bc9fbd9c1
Javier <Ibarrola>
2013-09-18 19:05:46 -0500

    created a new migration to add ids to comments

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130918212954) do
+ActiveRecord::Schema.define(version: 20130918235525) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,8 @@ ActiveRecord::Schema.define(version: 20130918212954) do
     t.text     "content"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "question_id"
+    t.integer  "answer_id"
   end
 
   create_table "questions", force: true do |t|
