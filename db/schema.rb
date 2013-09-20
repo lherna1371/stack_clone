085a786778abfd91b359e811165874ece3d7352d
Eugene Filimonov <eugenefilimonov@gmail.com>
2013-09-20 09:29:29 -0500

    answer voting with out tests"

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+ActiveRecord::Schema.define(version: 20130918235525) do
 
-# These are extensions that must be enabled in order to support this database
-# enable_extension "plpgsql"
-  
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "answers", force: true do |t|
     t.integer  "user_id"
