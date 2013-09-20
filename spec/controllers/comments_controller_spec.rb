3e9cddf53494042dbaea72bfc96e0bd8f72d4d7a
Javier <Ibarrola>
2013-09-19 19:01:40 -0500

    made a test for the model and added content presence validation

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -2,16 +2,19 @@ require 'spec_helper'
 
 describe CommentsController do
 
-	  	context 'when commenting on an answer' do
+	context 'when commenting on an answer' do
 		it 'loads the answer to post the comment to' do
+			# get :new
 			answer_moc = double(:answer, :id => 1)
+			
+			get new_answer_comment_path(answer_moc.id) 
 			Answer.should_receive(:find).with(answer_moc.id).and_return answer_moc
 
-			get new_answer_comment_path(answer_moc) 
-
-			assigns(:resource).should == answer_moc
-
+			# assigns(:resource).should == answer_moc
 		end
+
+
+		it 'redirects to questions' 
 	end
 
 end
