56934ba5f5fe92614f5e56149a1bb070a29d6f01
Eugene Filimonov <eugenefilimonov@gmail.com>
2013-09-21 13:43:18 -0500

    fixed nil error on sign in for is_active

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,17 +2,22 @@ class SessionsController < ApplicationController
 
   def create
   	@user = User.find_by_handle(params[:session][:handle])
-    if @user.is_active?
-      if @user != nil && @user.authenticate(params[:session][:password])
-        session[:user_id] = @user.id
-        redirect_to questions_path
-      else
-        flash[:error] = "Handle or Password is incorrect"
-        redirect_to login_path
-      end
+    if @user == nil
+      flash[:error] = "Handle or Password is incorrect"
+      redirect_to login_path
     else
-      @error = "Your account is not active"
-      render 'account/update'
+      if @user.is_active?
+        if  @user.authenticate(params[:session][:password])
+          session[:user_id] = @user.id
+          redirect_to questions_path, :notice => "Signed in!"
+        else
+          flash[:error] = "Handle or Password is incorrect"
+          redirect_to login_path, :notice => "Signed in!"
+        end
+      else
+        @error = "Your account is not active"
+        render 'account/update'
+      end
     end
   end
 
