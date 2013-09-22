9ee2e2d197fe155190bb8b0bfb0a7dfcb387ea57
Javier <Ibarrola>
2013-09-22 00:23:37 -0500

    added sign in via twitter

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -5,11 +5,32 @@ class AuthenticationsController < ApplicationController
 	end
 
 	def create
-	  auth = request.env["omniauth.auth"]
-	  current_user.authentications.find_or_create_by_provider_and_uid(auth['provider'], auth['uid'])
-	  flash[:notice] = "Authentication successful."
-	  redirect_to authentications_path
-	  # redirect_to questions_path
+		# render :text => omniauth.to_yaml
+		omniauth = request.env["omniauth.auth"]
+		provider = omniauth[:provider]
+		uid = omniauth[:uid]
+		nickname = omniauth[:info].nickname
+
+	  authentication = Authentication.find_by_provider_and_uid(provider, uid)
+	  if authentication
+	  	user = authentication.user
+	  	session[:user_id] = user.id
+      redirect_to questions_path, :notice => "Signed in succesfully!"
+	  elsif current_user
+	  	current_user.authentications.create!(:provider => provider, :uid => uid)
+	  	flash[:notice] = "Authentication successful."
+	  	redirect_to authentications_path
+	  else
+	  	@user = User.new(handle: nickname, email: "#{nickname}@twitter.com" ,password: uid ,password_confirmation: uid )
+	    user.authentications.create!(:provider => provider, :uid => uid)
+	    if user.save
+	    	session[:user_id] = @user.id
+	      flash[:notice] = "Signed in successfully."
+	      redirect_to questions_path, :notice => "Signed in succesfully!"
+	    else
+	      redirect_to :back, :notice => "Authentication failed try again"
+	    end
+		end
 	end
 
 	def destroy
