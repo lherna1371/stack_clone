a21c21be95c3caac55b73d55922440ef09396d5e
Luis Hernandez <lherna1371@gmail.com>
2013-09-22 13:55:38 -0500

    completed tag

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20130922022653) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "answers", force: true do |t|
     t.integer  "user_id"
     t.text     "content"
@@ -99,7 +102,6 @@ ActiveRecord::Schema.define(version: 20130922022653) do
     t.datetime "updated_at"
     t.boolean  "admin",           default: false
     t.string   "photo",           default: " "
-    t.boolean  "is_active",       default: true
   end
 
 end
