7308727badace8b580969d7e2f73f897f89c99a9
Javier <Ibarrola>
2013-09-22 13:09:07 -0500

    did first model test

--- spec/models/authentication_spec.rb
+++ spec/models/authentication_spec.rb
@@ -1,5 +1,15 @@
 require 'spec_helper'
 
 describe Authentication do
-  pending "add some examples to (or delete) #{__FILE__}"
+  	
+	let(:user){User.create!(handle: 'Javier', email: 'javier@dev.com', password: '1234', password_confirmation:'1234')}
+
+  	it "has an asisiacion whith user" do  
+		auth = user.authentications.create(provider: 'twitter', uid: 2)
+		expect(Authentication.last.provider).to eq 'twitter'
+	end
+
+
 end
+
+
