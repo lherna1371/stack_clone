263c782bf9c5241e2c0cf14f02454be05d91d0c8
Nicholas <nicholas.lee.3@gmail.com>
2013-09-21 19:14:00 -0500

    and other migrant table

--- db/migrate/20130921184336_create_upvote_questions.rb
+++
@@ -1,9 +0,0 @@
-class CreateUpvoteQuestions < ActiveRecord::Migration
-  def change
-    create_table :upvote_questions do |t|
-			t.integer :user_id
-			t.integer :question_id
-      t.timestamps
-    end
-  end
-end
