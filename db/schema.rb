fd8d505220eb34a7684cb31c6bdae696cc4dbff0
Eugene Filimonov <eugenefilimonov@gmail.com>
2013-09-23 08:06:42 -0500

    took out two lines form schema

--- db/schema.rb
+++ db/schema.rb
@@ -13,8 +13,7 @@
 
 ActiveRecord::Schema.define(version: 20130922022653) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+
 
   create_table "answers", force: true do |t|
     t.integer  "user_id"
